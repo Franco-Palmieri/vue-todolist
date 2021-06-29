e6615b35a2ca309f4316324e417fb5bc1bfc8602
Ncesecrede <francescopm95@gmail.com>
2021-06-29 13:31:42 +0100

    Todo List

---
+++ script.js
@@ -0,0 +1,18 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            todoList: [
+            "Fare i compiti",
+            "Fare la spesa",
+            "Fare il bucato"
+            ],
+            newTodoList: [],
+        },
+        methods:{
+            addNewList: function() {
+                
+            }
+        },
+    }
+);
